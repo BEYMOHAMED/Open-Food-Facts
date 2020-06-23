800c3c2973c7ff83eae9785b7782770d8c5c1f32
BEY BOUMEZRAG MOHAMED <linkthebrave86@gmail.com>
2020-06-23 10:39:04 +0200

    first course commit

--- app/src/main/java/fr/supinternet/mbeyboumezrag/MainActivity.kt
+++ app/src/main/java/fr/supinternet/mbeyboumezrag/MainActivity.kt
@@ -1,12 +1,18 @@
 package fr.supinternet.mbeyboumezrag
 
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.let {
+            it.setBackgroundDrawable(ColorDrawable(ContextCompat.getColor(this@MainActivity, android.R.color.transparent)))
+            it.title = ""
+        }
     }
 }
